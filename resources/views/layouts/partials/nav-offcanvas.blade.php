fb5e73687676030e9e05898a029d1347e7e9220a
ChrisKoivu <cmkoivu@comcast.net>
2018-10-12 19:26:48 -0400

    edited css stylesheets

--- resources/views/layouts/partials/nav-offcanvas.blade.php
+++ resources/views/layouts/partials/nav-offcanvas.blade.php
@@ -1,4 +1,4 @@
-<nav class="navbar navbar-expand-lg fixed-top navbar-dark bg-dark">
+<nav class="navbar navbar-expand-lg fixed-top navbar-light bg-light">
     <a class="navbar-brand mr-auto mr-lg-0" href="#">Offcanvas navbar</a>
     <button class="navbar-toggler p-0 border-0" type="button" data-toggle="offcanvas">
     <span class="navbar-toggler-icon"></span>
@@ -6,7 +6,7 @@
 
     <div class="navbar-collapse offcanvas-collapse" id="navbarsExampleDefault">
         <ul class="navbar-nav mr-auto">
-            <li class="nav-item active">
+            <li class="nav-item">
                 <a class="nav-link" href="#">Dashboard <span class="sr-only">(current)</span></a>
             </li>
 
@@ -39,9 +39,6 @@
             </li>
         @endguest
         </ul>
-        <form class="form-inline my-2 my-lg-0">
-            <input class="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search">
-            <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-        </form>
+        
     </div>
 </nav>
