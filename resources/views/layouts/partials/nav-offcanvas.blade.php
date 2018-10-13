91682f3472b60e55271654acd57eef8a7f6f8d0d
ChrisKoivu <cmkoivu@comcast.net>
2018-10-12 20:03:43 -0400

    created offcanvas navbar

--- resources/views/layouts/partials/nav-offcanvas.blade.php
+++ resources/views/layouts/partials/nav-offcanvas.blade.php
@@ -18,27 +18,22 @@
             <li class="nav-item"><a class="nav-link" href="{{ route('login') }}">Login</a></li>
             <li class="nav-item"><a class="nav-link" href="{{ route('register') }}">Register</a></li>
         @else
-            <li class="dropdown">
-                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false" aria-haspopup="true" v-pre>
-                    {{ Auth::user()->name }} <span class="caret"></span>
-                </a>
-
-                <ul class="dropdown-menu">
-                    <li>
-                        <a href="{{ route('logout') }}"
-                            onclick="event.preventDefault();
-                                     document.getElementById('logout-form').submit();">
-                            Logout
-                        </a>
-
-                        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                            {{ csrf_field() }}
-                        </form>
-                    </li>
-                </ul>
-            </li>
+         <li class="nav-item dropdown">
+                <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false" aria-haspopup="true" v-pre>
+                        {{ Auth::user()->name }} <span class="caret"></span>
+              </a>
+           <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+              <a class="dropdown-item" href="{{ route('logout') }}"
+                onclick="event.preventDefault();
+                 document.getElementById('logout-form').submit();">
+                 Logout  
+             </a>
+             <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                    {{ csrf_field() }}
+             </form>
+            </div>
+          </li>            
         @endguest
-        </ul>
-        
+      </ul>   
     </div>
 </nav>
