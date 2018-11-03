a632003ff56fa0a05db86a5626f83c240c582ee1
ChrisKoivu <cmkoivu@comcast.net>
2018-11-03 07:11:01 -0400

    created user controller, user resources and collection for api calls for json

--- app/Http/Resources/UserCollection.php
+++ app/Http/Resources/UserCollection.php
@@ -9,11 +9,16 @@ class UserCollection extends ResourceCollection
     /**
      * Transform the resource collection into an array.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param  \Illuminate\Http\Request
      * @return array
      */
     public function toArray($request)
     {
-        return parent::toArray($request);
+        return [
+            'data' => $this->collection,
+            'links' => [
+                'self' => 'link-value',
+            ],
+        ];
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,8 @@
 
 Auth::routes();
 
+
+
 Route::get('/', 'HomeController@index')->name('home');
 
 Route::get('/home', 'HomeController@index')->name('home');
@@ -27,3 +29,4 @@ Route::get('/admin', 'AdminController@admin')
     ->middleware('is_admin')    
     ->name('admin');
 
+    
