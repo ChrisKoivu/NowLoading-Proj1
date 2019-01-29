6be8f65eaaa2ba1f4876e96feb3c3ec6a02c38f2
unknown <cmkoivu@comcast.net>
2019-01-29 07:36:35 -0500

    merged branches

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,11 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
-<<<<<<< HEAD
-use Illuminate\Support\Facades\Schema; 
-=======
 use Illuminate\Support\Facades\Schema;
->>>>>>> reactjs
 
 class AppServiceProvider extends ServiceProvider
 {
