eeb49c8b60cfa0192c88071dad5b8317c9f97c80
unknown <cmkoivu@comcast.net>
2019-03-07 09:17:28 -0500

    created redirect from home page if survey not complete

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class HomeController extends Controller
 {
@@ -23,6 +24,11 @@ class HomeController extends Controller
      */
     public function index()
     {
+
+        $survey_complete = Auth::user()->survey_complete;
+        if(!$survey_complete){
+            return redirect()->route('survey');
+        }
         return view('home');
     }
 }
