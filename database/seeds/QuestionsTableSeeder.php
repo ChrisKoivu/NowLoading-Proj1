0ab5c531ac71efc339a6665af726489dd97b0dc9
ChrisKoivu <cmkoivu@comcast.net>
2019-03-12 08:20:24 -0400

    created 3 surveys, removed unused migration class

--- database/seeds/QuestionsTableSeeder.php
+++ database/seeds/QuestionsTableSeeder.php
@@ -15,42 +15,95 @@ class QuestionsTableSeeder extends Seeder
             'survey_question' => 'How can we help?',
             'created_at' => NULL,
             'updated_at' => NULL,
-        ]);
+        ]); //1
 
         DB::table('questions')->insert([
             'survey_question' => 'Is the individual you’re seeking to assist verbal or non-verbal?',
             'created_at' => NULL,
             'updated_at' => NULL,
-        ]);
+        ]); //2
 
         DB::table('questions')->insert([
             'survey_question' => 'How old is the individual you’re seeking to assist?',
             'created_at' => NULL,
             'updated_at' => NULL,
-        ]);
+        ]); //3
 
         DB::table('questions')->insert([
             'survey_question' => 'Is your child currently employed or seeking employment?',
             'created_at' => NULL,
             'updated_at' => NULL,
-        ]);
+        ]); //4
 
         DB::table('questions')->insert([
             'survey_question' => 'How can we help?',
             'created_at' => NULL,
             'updated_at' => NULL,
-        ]);
+        ]); //5
 
         DB::table('questions')->insert([
             'survey_question' => ' How old were you when diagnosed?',
             'created_at' => NULL,
             'updated_at' => NULL,
-        ]);
+        ]); //6
 
         DB::table('questions')->insert([
             'survey_question' => ' Are you currently employed or seeking employment?',
             'created_at' => NULL,
             'updated_at' => NULL,
-        ]);
+        ]); //7
+
+        DB::table('questions')->insert([
+            'survey_question' => 'How can we help?',
+            'created_at' => NULL,
+            'updated_at' => NULL,
+        ]); //8
+        
+        DB::table('questions')->insert([
+            'survey_question' => 'What industry is your business?',
+            'created_at' => NULL,
+            'updated_at' => NULL,
+        ]); //9
+
+        DB::table('questions')->insert([
+            'survey_question' => 'How can we help?',
+            'created_at' => NULL,
+            'updated_at' => NULL,
+        ]); //10
+
+        DB::table('questions')->insert([
+            'survey_question' => 'Is the individual you’re seeking to assist verbal or non-verbal?',
+            'created_at' => NULL,
+            'updated_at' => NULL,
+        ]); //11
+
+        DB::table('questions')->insert([
+            'survey_question' => 'What industry is your business?',
+            'created_at' => NULL,
+            'updated_at' => NULL,
+        ]); //12
+
+        DB::table('questions')->insert([
+            'survey_question' => 'How can we help?',
+            'created_at' => NULL,
+            'updated_at' => NULL,
+        ]); //13
+
+        DB::table('questions')->insert([
+            'survey_question' => 'How old is the individual you’re seeking to assist?',
+            'created_at' => NULL,
+            'updated_at' => NULL,
+        ]); //14
+
+        DB::table('questions')->insert([
+            'survey_question' => 'Is the individual you’re seeking to assist verbal or non-verbal?',
+            'created_at' => NULL,
+            'updated_at' => NULL,
+        ]); //15
+
+
+
+
+
     }
 }
--- database/seeds/ResponsesTableSeeder.php
+++ database/seeds/ResponsesTableSeeder.php
@@ -237,6 +237,378 @@ class ResponsesTableSeeder extends Seeder
             'updated_at' => NULL,
         ]);
 
+       
+        DB::table('responses')->insert([
+            'survey_question_response' => 'Some of our employees have children on the spectrum, seeking resources',
+            'question_id' =>8,
+            'created_at' => NULL,
+            'updated_at' => NULL,
+        ]);
+
+        DB::table('responses')->insert([
+            'survey_question_response' => 'Interested in hiring people on the spectrum',
+            'question_id' =>8,
+            'created_at' => NULL,
+            'updated_at' => NULL,
+        ]);
+
+        DB::table('responses')->insert([
+            'survey_question_response' => 'Interested in providing services or products to people on the spectrum',
+            'question_id' =>8,
+            'created_at' => NULL,
+            'updated_at' => NULL,
+        ]);
+
+        DB::table('responses')->insert([
+            'survey_question_response' => 'I supervise people on or may be on the spectrum',
+            'question_id' =>8,
+            'created_at' => NULL,
+            'updated_at' => NULL,
+        ]);
+
+        DB::table('responses')->insert([
+            'survey_question_response' => 'Financial/Banking',
+            'question_id' =>9,
+            'created_at' => NULL,
+            'updated_at' => NULL,
+        ]);
+
+        DB::table('responses')->insert([
+            'survey_question_response' => 'Government/Public/Social Services',
+            'question_id' =>9,
+            'created_at' => NULL,
+            'updated_at' => NULL,
+        ]);
+
+        DB::table('responses')->insert([
+            'survey_question_response' => 'Healthcare',
+            'question_id' =>9,
+            'created_at' => NULL,
+            'updated_at' => NULL,
+        ]);
+
+        DB::table('responses')->insert([
+            'survey_question_response' => 'Education',
+            'question_id' =>9,
+            'created_at' => NULL,
+            'updated_at' => NULL,
+        ]);
+
+        DB::table('responses')->insert([
+            'survey_question_response' => 'Legal',
+            'question_id' =>9,
+            'created_at' => NULL,
+            'updated_at' => NULL,
+        ]);
+
+        DB::table('responses')->insert([
+            'survey_question_response' => 'Manufacturing',
+            'question_id' =>9,
+            'created_at' => NULL,
+            'updated_at' => NULL,
+        ]);
+
+        DB::table('responses')->insert([
+            'survey_question_response' => 'Retail',
+            'question_id' =>9,
+            'created_at' => NULL,
+            'updated_at' => NULL,
+        ]);
+
+        DB::table('responses')->insert([
+            'survey_question_response' => 'Internet/Media/Advertising',
+            'question_id' =>9,
+            'created_at' => NULL,
+            'updated_at' => NULL,
+        ]);
+
+        DB::table('responses')->insert([
+            'survey_question_response' => 'Hospitality/Travel',
+            'question_id' =>9,
+            'created_at' => NULL,
+            'updated_at' => NULL,
+        ]);
+
+        DB::table('responses')->insert([
+            'survey_question_response' => 'Transportation',
+            'question_id' =>9,
+            'created_at' => NULL,
+            'updated_at' => NULL,
+        ]);
+
+        DB::table('responses')->insert([
+            'survey_question_response' => 'Art/Entertainment',
+            'question_id' =>9,
+            'created_at' => NULL,
+            'updated_at' => NULL,
+        ]);
+
+        DB::table('responses')->insert([
+            'survey_question_response' => 'Information Technology',
+            'question_id' =>9,
+            'created_at' => NULL,
+            'updated_at' => NULL,
+        ]);
+
+        DB::table('responses')->insert([
+            'survey_question_response' => 'Human Resources/Staffing',
+            'question_id' =>9,
+            'created_at' => NULL,
+            'updated_at' => NULL,
+        ]);
+
+        DB::table('responses')->insert([
+            'survey_question_response' => 'Other',
+            'question_id' =>9,
+            'created_at' => NULL,
+            'updated_at' => NULL,
+        ]);
+
+        DB::table('responses')->insert([
+            'survey_question_response' => 'Interested in providing services or products to people on the spectrum',
+            'question_id' =>10,
+            'created_at' => NULL,
+            'updated_at' => NULL,
+        ]);
+
+        DB::table('responses')->insert([
+            'survey_question_response' => 'I supervise people on or may be on the spectrum',
+            'question_id' =>10,
+            'created_at' => NULL,
+            'updated_at' => NULL,
+        ]);
+
+        DB::table('responses')->insert([
+            'survey_question_response' => 'Some of my clients are on the spectrum, seeking resources',
+            'question_id' =>10,
+            'created_at' => NULL,
+            'updated_at' => NULL,
+        ]);
+
+        DB::table('responses')->insert([
+            'survey_question_response' => 'Some of my clients have child(ren) on the spectrum, seeking resources',
+            'question_id' =>10,
+            'created_at' => NULL,
+            'updated_at' => NULL,
+        ]);
+
+        DB::table('responses')->insert([
+            'survey_question_response' => 'Verbal',
+            'question_id' =>11,
+            'created_at' => NULL,
+            'updated_at' => NULL,
+        ]);
+
+        DB::table('responses')->insert([
+            'survey_question_response' => 'Non-verbal',
+            'question_id' =>11,
+            'created_at' => NULL,
+            'updated_at' => NULL,
+        ]);
+
+        DB::table('responses')->insert([
+            'survey_question_response' => 'Both',
+            'question_id' =>11,
+            'created_at' => NULL,
+            'updated_at' => NULL,
+        ]);
+
+        DB::table('responses')->insert([
+            'survey_question_response' => 'Not sure',
+            'question_id' =>11,
+            'created_at' => NULL,
+            'updated_at' => NULL,
+        ]);
+
+        DB::table('responses')->insert([
+            'survey_question_response' => 'Financial/Banking',
+            'question_id' =>12,
+            'created_at' => NULL,
+            'updated_at' => NULL,
+        ]);
+
+        DB::table('responses')->insert([
+            'survey_question_response' => 'Government/Public/Social Services',
+            'question_id' =>12,
+            'created_at' => NULL,
+            'updated_at' => NULL,
+        ]);
+
+        DB::table('responses')->insert([
+            'survey_question_response' => 'Healthcare',
+            'question_id' =>12,
+            'created_at' => NULL,
+            'updated_at' => NULL,
+        ]);
+
+        DB::table('responses')->insert([
+            'survey_question_response' => 'Education',
+            'question_id' =>12,
+            'created_at' => NULL,
+            'updated_at' => NULL,
+        ]);
+
+        DB::table('responses')->insert([
+            'survey_question_response' => 'Legal',
+            'question_id' =>12,
+            'created_at' => NULL,
+            'updated_at' => NULL,
+        ]);
+
+        DB::table('responses')->insert([
+            'survey_question_response' => 'Manufacturing',
+            'question_id' =>12,
+            'created_at' => NULL,
+            'updated_at' => NULL,
+        ]);
+
+        DB::table('responses')->insert([
+            'survey_question_response' => 'Retail',
+            'question_id' =>12,
+            'created_at' => NULL,
+            'updated_at' => NULL,
+        ]);
+
+        DB::table('responses')->insert([
+            'survey_question_response' => 'Internet/Media/Advertising',
+            'question_id' =>12,
+            'created_at' => NULL,
+            'updated_at' => NULL,
+        ]);
+
+        DB::table('responses')->insert([
+            'survey_question_response' => 'Hospitality/Travel',
+            'question_id' =>12,
+            'created_at' => NULL,
+            'updated_at' => NULL,
+        ]);
+
+        DB::table('responses')->insert([
+            'survey_question_response' => 'Transportation',
+            'question_id' =>12,
+            'created_at' => NULL,
+            'updated_at' => NULL,
+        ]);
+
+        DB::table('responses')->insert([
+            'survey_question_response' => 'Art/Entertainment',
+            'question_id' =>12,
+            'created_at' => NULL,
+            'updated_at' => NULL,
+        ]);
+
+        DB::table('responses')->insert([
+            'survey_question_response' => 'Information Technology',
+            'question_id' =>12,
+            'created_at' => NULL,
+            'updated_at' => NULL,
+        ]);
+
+        DB::table('responses')->insert([
+            'survey_question_response' => 'Human Resources/Staffing',
+            'question_id' =>12,
+            'created_at' => NULL,
+            'updated_at' => NULL,
+        ]);
+
+        DB::table('responses')->insert([
+            'survey_question_response' => 'Other',
+            'question_id' =>12,
+            'created_at' => NULL,
+            'updated_at' => NULL,
+        ]);
+
+        DB::table('responses')->insert([
+            'survey_question_response' => 'My family member, neighbor, or friend has child(ren) on the spectrum, 
+            seeking information',
+            'question_id' =>13,
+            'created_at' => NULL,
+            'updated_at' => NULL,
+        ]);
+
+        DB::table('responses')->insert([
+            'survey_question_response' => 'I work with people on the spectrum, seeking information',
+            'question_id' =>13,
+            'created_at' => NULL,
+            'updated_at' => NULL,
+        ]);
+
+        DB::table('responses')->insert([
+            'survey_question_response' => 'Interested in working with people on the spectrum, seeking information',
+            'question_id' =>13,
+            'created_at' => NULL,
+            'updated_at' => NULL,
+        ]);
+
+        DB::table('responses')->insert([
+            'survey_question_response' => 'General, seeking more information',
+            'question_id' =>13,
+            'created_at' => NULL,
+            'updated_at' => NULL,
+        ]);
+
+        DB::table('responses')->insert([
+            'survey_question_response' => 'Birth to 3 years of age',
+            'question_id' =>14,
+            'created_at' => NULL,
+            'updated_at' => NULL,
+        ]);
+
+        DB::table('responses')->insert([
+            'survey_question_response' => 'Preschool',
+            'question_id' =>14,
+            'created_at' => NULL,
+            'updated_at' => NULL,
+        ]);
+
+        DB::table('responses')->insert([
+            'survey_question_response' => 'Elementary School',
+            'question_id' =>14,
+            'created_at' => NULL,
+            'updated_at' => NULL,
+        ]);
+
+        DB::table('responses')->insert([
+            'survey_question_response' => 'Middle School',
+            'question_id' =>14,
+            'created_at' => NULL,
+            'updated_at' => NULL,
+        ]);
+
+        DB::table('responses')->insert([
+            'survey_question_response' => 'High School',
+            'question_id' =>14,
+            'created_at' => NULL,
+            'updated_at' => NULL,
+        ]);
+
+        DB::table('responses')->insert([
+            'survey_question_response' => 'Adult',
+            'question_id' =>14,
+            'created_at' => NULL,
+            'updated_at' => NULL,
+        ]);
+
+        DB::table('responses')->insert([
+            'survey_question_response' => 'Verbal',
+            'question_id' =>15,
+            'created_at' => NULL,
+            'updated_at' => NULL,
+        ]);
+
+        DB::table('responses')->insert([
+            'survey_question_response' => 'Non-verbal',
+            'question_id' =>15,
+            'created_at' => NULL,
+            'updated_at' => NULL,
+        ]);
+
+        DB::table('responses')->insert([
+            'survey_question_response' => 'Not sure',
+            'question_id' =>15,
+            'created_at' => NULL,
+            'updated_at' => NULL,
+        ]);
 
     }
 }
