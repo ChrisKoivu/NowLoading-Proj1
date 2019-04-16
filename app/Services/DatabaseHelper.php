b984f8e9edde08e230f4fc7f3c7687884c73c21b
chriskoivu <cmkoivu@comcast.net>
2019-04-16 17:16:09 -0400

    added new queries to database helper

--- app/Services/DatabaseHelper.php
+++ app/Services/DatabaseHelper.php
@@ -11,13 +11,13 @@ class DatabaseHelper
    public function getResponseTotalsCaregiver(){
 
       $response_totals = 
-      DB::select(DB::raw("SELECT count(choices.response_id), responses.survey_question_response
+      DB::select(DB::raw("SELECT count(choices.response_id), questions.survey_question, responses.survey_question_response
       FROM `choices`
       INNER JOIN responses on responses.id = choices.response_id
       INNER JOIN questions on questions.id = choices.question_id
       inner JOIN question_survey_type on questions.id = question_survey_type.question_id
       where question_survey_type.survey_type_id = 1
-      group by responses.survey_question_response, choices.response_id"));
+      group by responses.survey_question_response, questions.survey_question, choices.response_id"));
     
       return $response_totals;
 
@@ -28,13 +28,13 @@ class DatabaseHelper
    public function getResponseTotalsSpectrum(){
 
       $response_totals = 
-      DB::select(DB::raw("SELECT count(choices.response_id), responses.survey_question_response
+      DB::select(DB::raw("SELECT count(choices.response_id), questions.survey_question, responses.survey_question_response
       FROM `choices`
       INNER JOIN responses on responses.id = choices.response_id
       INNER JOIN questions on questions.id = choices.question_id
       inner JOIN question_survey_type on questions.id = question_survey_type.question_id
       where question_survey_type.survey_type_id = 2
-      group by responses.survey_question_response, choices.response_id"));
+      group by responses.survey_question_response, questions.survey_question, choices.response_id"));
     
       return $response_totals;
 
@@ -45,13 +45,13 @@ class DatabaseHelper
    public function getResponseTotalsEmployer(){
 
       $response_totals = 
-      DB::select(DB::raw("SELECT count(choices.response_id), responses.survey_question_response
+      DB::select(DB::raw("SELECT count(choices.response_id), questions.survey_question, responses.survey_question_response
       FROM `choices`
       INNER JOIN responses on responses.id = choices.response_id
       INNER JOIN questions on questions.id = choices.question_id
       inner JOIN question_survey_type on questions.id = question_survey_type.question_id
       where question_survey_type.survey_type_id = 3
-      group by responses.survey_question_response, choices.response_id"));
+      group by responses.survey_question_response, questions.survey_question, choices.response_id"));
     
       return $response_totals;
 
@@ -62,13 +62,13 @@ class DatabaseHelper
    public function getResponseTotalsProfessional(){
 
       $response_totals = 
-      DB::select(DB::raw("SELECT count(choices.response_id), responses.survey_question_response
+      DB::select(DB::raw("SELECT count(choices.response_id), questions.survey_question, responses.survey_question_response
       FROM `choices`
       INNER JOIN responses on responses.id = choices.response_id
       INNER JOIN questions on questions.id = choices.question_id
       inner JOIN question_survey_type on questions.id = question_survey_type.question_id
       where question_survey_type.survey_type_id = 4
-      group by responses.survey_question_response, choices.response_id"));
+      group by responses.survey_question_response, questions.survey_question, choices.response_id"));
     
       return $response_totals;
 
@@ -79,13 +79,13 @@ class DatabaseHelper
    public function getResponseTotalsCommunity(){
 
       $response_totals = 
-      DB::select(DB::raw("SELECT count(choices.response_id), responses.survey_question_response
+      DB::select(DB::raw("SELECT count(choices.response_id), questions.survey_question, responses.survey_question_response
       FROM `choices`
       INNER JOIN responses on responses.id = choices.response_id
       INNER JOIN questions on questions.id = choices.question_id
       inner JOIN question_survey_type on questions.id = question_survey_type.question_id
       where question_survey_type.survey_type_id = 5
-      group by responses.survey_question_response, choices.response_id"));
+      group by responses.survey_question_response, questions.survey_question, choices.response_id"));
     
       return $response_totals;
 
@@ -93,6 +93,15 @@ class DatabaseHelper
                 
    }
 
+   public function getProfessionTotalsByZipCode(){
+      $response_totals = 
+      DB::select(DB::raw("SELECT count(demographics.profession), demographics.zip, demographics.profession FROM `demographics`
+      group by demographics.profession, demographics.zip"));
+    
+      return $response_totals;
+
+
+   }
 
 
 
