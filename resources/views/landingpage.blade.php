2e23e420156af3b3818d9dc05b8e889c8d72c54d
Mason <madman3161@gmail.com>
2019-04-21 17:10:57 -0400

    landingpage created

---
+++ resources/views/landingpage.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+@extends('layouts.app')
+@section('content')
+<html lang="en-US" xml:lang="en-US">
+    <header class="header">
+        <div class="col-lg-12 text-center">
+            <a href="https://makingstridesforautism.org/index.html" tabindex="-1" target="_blank"><img src="{{url('images/ms4a-sta-logo.png')}}" class="resize center"  alt="MS4A Logo"></a>
+        </div>
+    </header>  
+    <body>
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-md-16">
+                    <div class="col-md-16 col-md-offset-4">
+                        <a class="btn btn-primary" href="">Login</a>
+                        <a class="btn btn-primary" href="">Register</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </body>
+    <footer class="footer">
+    </footer>
+</html>
+@endsection
